bb14a2aefa66942f4f78e5badf7628bb392006bf
Your Name <ada55@gmail.com>
2023-03-24 16:01:59 +0000

    smoth scoling

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-const items = document.querySelectorAll(".item");
+// const items = document.querySelectorAll(".item");
 // const iconopen = document.getElementById("icon-open");
 // const hiddennavbar = document.querySelector(".hiddennavbar");
 // const iconclose = document.getElementById("icon-close");
 // const fas = document.querySelector(".fas");
 
-items.forEach((item) => {
-  item.addEventListener("mouseover", () => {
-    item.classList.add("active");
-  });
-  item.addEventListener("mouseout", () => {
-    item.classList.remove("active");
-  });
-});
+// items.forEach((item) => {
+//   item.addEventListener("mouseover", () => {
+//     item.classList.add("active");
+//   });
+//   item.addEventListener("mouseout", () => {
+//     item.classList.remove("active");
+//   });
+// });
 
 // iconopen.addEventListener("click", () => {
 //   hiddennavbar.classList.add("active-navbar");
@@ -20,3 +20,18 @@ items.forEach((item) => {
 // iconclose.addEventListener("click", () => {
 //   hiddennavbar.classList.remove("active-navbar");
 // });
+
+// smoth scroling
+const observer = new IntersectionObserver(function (entries) {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenelemnts = document.querySelectorAll(".hidden");
+hiddenelemnts.forEach((el) => observer.observe(el));
